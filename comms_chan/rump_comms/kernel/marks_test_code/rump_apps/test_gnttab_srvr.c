83928ed82dc70654fe276198b23029167971abdd
Mark Mason <markmason@Marks-MBP.grandenetworks.net>
2016-06-13 12:17:40 -0500

    Removed unneeded headers and comments

--- comms_chan/rump_comms/kernel/marks_test_code/rump_apps/test_gnttab_srvr.c
+++ comms_chan/rump_comms/kernel/marks_test_code/rump_apps/test_gnttab_srvr.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-
-#define STREAMLEN 7 
-#define HELLO_STRING "Hello\n"
 
 int main(int argc, char **argv) {
 
@@ -16,34 +6,6 @@ int main(int argc, char **argv) {
 
 		sleep(2);
 	}
-	/*
-	struct sockaddr_in s_server;
-	int sd;
-	char buf[STREAMLEN];
-	
-	if( (sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ) {
-		fprintf(stderr, "Failed to create socket...\n");
-
-		return -1;
-	}
-
-	memset(&s_server, 0, sizeof(s_server));
-	
-	s_server.sin_family = AF_INET	;
-	s_server.sin_addr.s_addr = inet_addr(argv[1]);
-	s_server.sin_port = htons(atoi(argv[2]));
-
-	connect(sd, (struct sockaddr *)&s_server, sizeof(s_server));
-
-	memset(buf, 0, STREAMLEN);
-	memcpy(buf,HELLO_STRING,STREAMLEN);
-
-	send(sd, buf, STREAMLEN, 0);
-
-	fprintf(stdout, "Sent the string over the socket...\n");
-
-	close(sd);
-	*/
 
 	return 0;
 }
