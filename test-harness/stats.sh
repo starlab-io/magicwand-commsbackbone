0720099b0b46e368a07ec34d20a22a59e146e348
Patrick Dwyer <patrick.dwyer@invincea.com>
2016-06-13 10:24:39 -0400

    Attempting to fix issue with docker-compose failing during stats collection for test-harness

--- test-harness/stats.sh
+++ test-harness/stats.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-COUNT=`docker-compose exec apache ps -aux | grep httpd | wc -l`
+COUNT=`docker-compose exec -T apache ps -aux | grep httpd | wc -l`
 echo "=$COUNT streams active in Apache"
