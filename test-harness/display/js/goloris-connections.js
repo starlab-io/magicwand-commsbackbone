22419e5a7ea17a1757a2bd261cb2258e1029323c
Patrick Dwyer <patrick.dwyer@invincea.com>
2016-07-08 14:15:34 -0400

    Graph the min/mean/max of the connections held by GoLoris

--- test-harness/display/js/goloris-connections.js
+++ test-harness/display/js/goloris-connections.js
@@ -1,17 +1,17 @@
-
-// GoLoris connection graph
 (function () {
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 200 - margin.top - margin.bottom;
 
-var formatDate = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
+var parseDate = d3.timeParse("%Y-%m-%dT%H:%M:%SZ");
 
 var x = d3.scaleTime()
     .range([0, width]);
 
 var y = d3.scaleLinear()
     .range([height, 0]);
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 var xAxis = d3.axisBottom()
     .scale(x);
@@ -20,29 +20,50 @@ var yAxis = d3.axisLeft()
     .scale(y);
 
 var line = d3.line()
-    .x(function(d) { return x(formatDate(d.timestamp)); })
-    .y(function(d) { return y(d.avg); });
+    .curve(d3.curveBasis)
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.rate); });
 
-var httperfSvg = d3.select("#goloris-connections-svg").append("svg")
+var svg = d3.select("#goloris-connections-svg").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// we know from the app layer which test we're trying to view
 d3.json("/data/goloris/" + window.application.query.test + ".json", function(error, data) {
   if (error) throw error;
-console.log("Got goloris data...");
-console.log(data);
-  x.domain(d3.extent(data.aggregate, function(d) { return formatDate(d.timestamp); }));
-  y.domain(d3.extent(data.aggregate, function(d) { return d.avg; }));
 
-  httperfSvg.append("g")
+  color.domain(["min", "avg", "max"]);
+
+  data.aggregate.forEach(function(d) {
+    d.date = parseDate(d.timestamp);
+  });
+
+  var cities = color.domain().map(function(name) {
+  console.log("mapping ["+ name + "]");
+    var blob = {
+      name: name,
+      values: data.aggregate.map(function(d) {
+        return {date: d.date, rate: +d[name]};
+      })
+    };
+    console.log(blob);
+    return blob;
+  });
+
+  x.domain(d3.extent(data.aggregate, function(d) { return d.date; }));
+
+  y.domain([
+    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.rate; }); }),
+    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.rate; }); })
+  ]);
+
+  svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
 
-  httperfSvg.append("g")
+  svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
     .append("text")
@@ -50,12 +71,23 @@ console.log(data);
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Reply-Rate");
+      .text("Temperature (ºF)");
 
-  httperfSvg.append("path")
-      .datum(data.aggregate)
+  var city = svg.selectAll(".city")
+      .data(cities)
+    .enter().append("g")
+      .attr("class", "city");
+
+  city.append("path")
       .attr("class", "line")
-      .attr("d", line);
-});
+      .attr("d", function(d) { console.log(d); return line(d.values); })
+      .style("stroke", function(d) { return color(d.name); });
 
+  city.append("text")
+      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.rate) + ")"; })
+      .attr("x", -100)
+      .attr("dy", "-1.35em")
+      .text(function(d) { return d.name; });
+});
 })(this);
