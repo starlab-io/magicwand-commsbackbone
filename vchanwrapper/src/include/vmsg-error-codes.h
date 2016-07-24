1d3f664b2c13294e629748656897311154b416ff
Scott Wimer <scott.wimer@gordianboxes.com>
2016-07-24 00:27:30 -0700

    Add more error codes

--- vchanwrapper/src/include/vmsg-error-codes.h
+++ vchanwrapper/src/include/vmsg-error-codes.h
@@ -11,10 +11,9 @@ enum vmsg_error_code {
 	E_VMSG_UNINITIALIZED,		// A vmsg was used before it was ready.
 	E_VMSG_WRONG_TYPE_FAMILY,	// Did not have expected type family.
 	E_VMSG_INVALID_TYPE,		// Supplied vmsg type not valid.
-	E_VMSG_INVALID_SIZE,		// Supplied size not valid for type.
-	E_VMSG_
-	E_VMSG_
-	E_VMSG_
+	E_VMSG_INVALID_SIZE_FOR_TYPE,	// Supplied size not valid for the type.
+	E_VMSG_SIZE_TOO_LARGE,		// Supplied size exceeds max size.
 
-	E_VMSG_MAX_ERROR_CODE_VALUE
+	E_VMSG_MAX_ERROR_CODE_VALUE,
+	E_SUCCESS = 0,
 }
