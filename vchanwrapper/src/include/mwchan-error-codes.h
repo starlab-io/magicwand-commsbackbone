02b7efe0bfaa54b0114194131c3101a6779c17f1
Scott Wimer <scott.wimer@gordianboxes.com>
2016-07-27 01:22:35 -0700

    Update the error codes

--- vchanwrapper/src/include/mwchan-error-codes.h
+++ vchanwrapper/src/include/mwchan-error-codes.h
@@ -1,20 +1,32 @@
+#ifndef __MWCHAN_ERROR_CODES_H__
+#define __MWCHAN_ERROR_CODES_H__
 // @MAGICWAND_HEADER@
 
 /**
  * Error codes used by mwchan.
  * The error codes in mwchan are always negative numbers.
- * The mwchan error codes start at -20,000 and go up.
- * The error code space -20,000 through -19,000 is reserved for mwchan errors.
+ * 0 indicates success rather than an error.
+ * The available range of error code values is from E_VMSG_RANGE_MIN to
+ * E_VMSG_RANGE_MAX.  The defined values range from E_VMSG_RANGE_MIN + 1
+ * to E_MWCHAN_ERROR_CODE_LIMIT - 1.
  */
+#define E_VMSG_RANGE_MIN -20999
+#define E_VMSG_RANGE_MAX -20000
 enum mwchan_error_code {
-	E_MWCHAN_NULL_POINTER = -20000,     // A pointer was null.
-	E_MWCHAN_CLOSED,                    // The struct mwchan was closed.
-	E_MWCHAN_MALFORMED,                 // The struct mwchan is malformed.
-	E_MWCHAN_MALFORMED_VMSG,            // The struct vmsg is malformed.
-	E_MWCHAN_ALLOC_FAILED,              // An allocation failed.
+	E_MWCHAN_ERROR_CODE_MIN = E_VMSG_RANGE_MIN,
+	E_MWCHAN_NULL_POINTER,    // A pointer was null.
+	E_MWCHAN_CLOSED,          // The struct mwchan was closed.
+	E_MWCHAN_MALFORMED,       // The struct mwchan is malformed.
+	E_MWCHAN_MALFORMED_VMSG,  // The struct vmsg is malformed.
+	E_MWCHAN_ALLOC_FAILED,    // An allocation failed.
+	E_MWCHAN_WRITE_FAILED,    // Writing to the underlying vchan failed.
+	E_MWCHAN_READ_FAILED,     // Reading from the underlying vchan failed.
+	E_MWCHAN_TOO_LARGE,       // Message too large for destination.
+	E_MWCHAN_NO_DATA,         // No data available in underlying vchan.
 
-	// All error codes are > -20,000 and < E_MWCHAN_MAX_ERROR_CODE_VALUE.
-	E_MWCHAN_MAX_ERROR_CODE_VALUE
+	E_MWCHAN_ERROR_CODE_LIMIT,
+	E_MWCHAN_ERROR_CODE_MAX = E_VMSG_RANGE_MAX,
+	E_SUCCESS = 0
 };
 
-
+#endif // __MWCHAN_ERROR_CODES_H__
--- vchanwrapper/src/include/vmsg-error-codes.h
+++ vchanwrapper/src/include/vmsg-error-codes.h
@@ -1,19 +1,30 @@
+#ifndef __VMST_ERROR_CODES_H__
+#define __VMST_ERROR_CODES_H__
 // @MAGICWAND_HEADER@
 
 /**
  * Error codes used by vmsg.
  * The error codes are always negative numbers.
- * The vmsg error codes start at -16,000 and go up.
- * The error code space -16,000 through -15,000 is reserved for vmsg errors.
+ * 0 indicates success rather than an error.
+ * The available range of error code values is from E_VMSG_RANGE_MIN to
+ * E_VMSG_RANGE_MAX.  The defined values range from E_VMSG_RANGE_MIN + 1 to
+ * E_VMSG_ERROR_CODE_LIMIT - 1.
  */
+#define E_VMSG_RANGE_MIN -16999
+#define E_VMSG_RANGE_MAX -16000
 enum vmsg_error_code {
-	E_VMSG_NULL_POINTER = -16000,	// A pointer was NULL.
+	E_VMSG_ERROR_CODE_MIN = E_VMSG_RANGE_MIN,
+	E_VMSG_NULL_POINTER,         	// A pointer was NULL.
 	E_VMSG_UNINITIALIZED,		// A vmsg was used before it was ready.
 	E_VMSG_WRONG_TYPE_FAMILY,	// Did not have expected type family.
 	E_VMSG_INVALID_TYPE,		// Supplied vmsg type not valid.
 	E_VMSG_INVALID_SIZE_FOR_TYPE,	// Supplied size not valid for the type.
 	E_VMSG_SIZE_TOO_LARGE,		// Supplied size exceeds max size.
 
-	E_VMSG_MAX_ERROR_CODE_VALUE,
-	E_SUCCESS = 0,
+	E_VMSG_ERROR_CODE_LIMIT,
+	E_VMSG_ERROR_CODE_MAX = E_VMSG_RANGE_MAX,
+	E_SUCCESS = 0
 }
+
+#endif // __VMST_ERROR_CODES_H__
+
