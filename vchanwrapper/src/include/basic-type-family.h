0f01926214cba33df141293e3ccc9762e4a8d658
Scott Wimer <scott.wimer@gordianboxes.com>
2016-07-31 00:42:58 -0700

    Improved docs; small enum cleanup

--- vchanwrapper/src/include/basic-type-family.h
+++ vchanwrapper/src/include/basic-type-family.h
@@ -6,12 +6,19 @@
  * The "basic" vmsg type-family provides 10 bits for the message type.
  * This header specifies the pre-existing basic message type values.
  *
- * TODO: Need to define the actual format of these message types somewhere.
+ * Basic Type Family Message Header
+ *  * First two bits must be set to:  00
+ *  * Next 10 bits specify message type [t]
+ *  * Final 20 bits specify length of the Value [s]
+ *
+ * Basic Type Family Header Format:
+ *   Bit position:  12345678 12345678 12345678 12345678
+ *   Value / type:  00tttttt ttttssss ssssssss ssssssss
  */
 typedef enum vmsg_basic_type {
 	//! Not really a type -- just compiler tricks.
 	// This makes sure the compiler will use a signed int for our enum.
-	// The signed int is necessary so that enum_vmsg_error_code values
+	// The signed int is necessary so that enum mwchan_error_code values
 	// can "sort of" occupy the same data space as the basic types.
 	BMT_ERROR  = -1,
 
@@ -26,12 +33,12 @@ typedef enum vmsg_basic_type {
 	//! Record from vmstat.
 	BMT_TYPE_SYS_VMSTAT_RECORD = 3,
 
-	// Finally, reserved space for custom types.
+	// Reserved space for custom types.
 	//! Custom type values should be greater than this.
 	BMT_TYPE_CUSTOM_TYPES_START = 255,
 
 	//! Valid types must be less than this.
-	BMT_TYPE_LIMIT = 1024
+	BMT_TYPE_MAX = 1023 	// (2 << 10) - 1
 } vmsg_basic_type_t;
 
 #endif // __BASIC_TYPE_FAMILY_H__
