fd64f0e81ba0e5c69044f35ad7aeebb9aba7e34c
Mark <mark.mason@starlab.io>
2016-11-11 20:36:15 -0600

    Added concrete sock_info list.

---
+++ protvm/user/tcp_ip_mlt_thrd/test_tcpip_app.c
@@ -0,0 +1,110 @@
+/*************************************************************************
+* STAR LAB PROPRIETARY & CONFIDENTIAL
+* Copyright (C) 2016, Star Lab — All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited.
+***************************************************************************/
+
+/**
+ * @file    test_server.c
+ * @author  Mark Mason 
+ * @date    10 September 2016
+ * @version 0.1
+ * @brief   A simple test server to test comms.
+ */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <pthread.h>
+#include <unistd.h>
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+void *
+thread_func( void *data )
+{
+    int                socket_desc;
+    int                err = 0;
+
+    // 1> Call Socket
+    socket_desc = socket( AF_INET, SOCK_STREAM, 0 );
+     
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+        err = 1;
+        goto ErrorExit;
+
+    } else {
+        printf("Socket Number: %d\n", socket_desc);
+    }
+    
+    // 2> Call close
+    close(socket_desc);
+
+ErrorExit:
+    return;
+}
+
+int main(int argc , char *argv[])
+{
+    int                socket_desc;
+    struct sockaddr_in server;
+    int                err = 0;
+
+    char               client_message[20];
+    char              *hello = "Hello\n";
+
+    pthread_t          thr;
+
+    memset( client_message, 0, 20 );
+    strcpy( client_message, hello );
+
+    // 1> Call Socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+     
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+        err = 1;
+        goto ErrorExit;
+
+    } else {
+        printf("Socket Number: %d\n", socket_desc);
+    }
+
+    // 2> Call connect
+
+    server.sin_addr.s_addr = inet_addr("192.168.0.8");
+    server.sin_family = AF_INET;
+    //server.sin_port = htons( 8888 );
+    server.sin_port = htons( 21845);
+
+    if (connect(socket_desc, (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        printf("connect failed. Error");
+        return 1;
+    }
+
+    printf("Connected\n");
+
+    // 3> Call write
+    send(socket_desc, client_message , strlen(client_message), 0);
+
+    // 4> Call close
+    close(socket_desc);
+
+    printf("\n\nCalling Thread Function\n\n");
+
+    if ( pthread_create( &thr, NULL, thread_func, NULL )  != 0 )
+        exit(1);
+
+    pthread_join( thr, NULL );
+     
+ErrorExit:
+    return err;
+}
