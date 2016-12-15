8f732495c6a58cf54e2f09d60d7bef26158117a3
Alex Speasmaker <alex.speasmaker@starlab.io>
2016-12-15 15:11:05 -0600

    rename xenevent scripts run -> debug.sh perf_run -> run it just makes more sensegit status

--- unikernel/rump-xenevent/apps/xenevent_app/run.sh
+++ unikernel/rump-xenevent/apps/xenevent_app/run.sh
@@ -4,18 +4,17 @@
 ## Refer to setup_net.sh 
 ##
 
-echo "If debugging, use:"
-echo "gdb -ex 'target remote:1234' xenevent.run"
+echo "Running xenvent without debugging enabled"
 
 #sudo ../rumprun/bin/rumprun xen -di -I xen0,xenif -W xen0,inet,static,10.190.2.24/24 
-IP=192.168.0.18
+IP=10.1.2.18
 
 # rumprun -S xen -dip -D 1234 -M 128 -N client-rump \
 #         -I if,tap0,'-net tap,script=no,ifname=tap0' \
 #         -W if,inet,static,10.0.120.101/24 \
 #         client.run
 
-rumprun -S xen -dip -D 1234 -M 512 -N xenevent-rump \
+rumprun -S xen -di -M 256 -N xenevent-rump \
         -I xen0,xenif \
-        -W xen0,inet,static,$IP/24 \
+        -W xen0,inet,$IP/24 \
         xenevent.run
