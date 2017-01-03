907ef48507bd09a7a7fb1d8ef98c55e3b52a5de5
Alex Speasmaker <alex.speasmaker@starlab.io>
2017-01-03 11:07:48 -0600

    Added mt translations to translate.c

--- common/translate.h
+++ common/translate.h
@@ -8,8 +8,16 @@
 void
 populate_sockaddr_in( struct sockaddr_in * sockaddr,
 		              mt_sockaddr_in_t *mt_sockaddr); 
-	
 void
 populate_mt_sockaddr_in( mt_sockaddr_in_t * mt_sockaddr,
 						 struct sockaddr_in * SockAddr );
+int
+xe_net_get_native_sock_type( mt_sock_type_t Type );
+
+int 
+xe_net_get_native_protocol_family( mt_protocol_family_t Fam );
+
+mt_protocol_family_t
+xe_net_get_mt_protocol_family( sa_family_t Fam );
+
 #endif
