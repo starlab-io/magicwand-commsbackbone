edd9887d93a56088ae2cf13cd94a44783a523b40
Alex Speasmaker <alex.speasmaker@starlab.io>
2017-01-03 11:07:48 -0600

    Added mt translations to translate.c

--- common/translate.c
+++ common/translate.c
@@ -3,14 +3,86 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <stdio.h>
 
 #include "message_types.h"
 #include "translate.h"
+
+
+mt_protocol_family_t
+xe_net_get_mt_protocol_family( sa_family_t Fam )
+{
+    mt_protocol_family_t mt_fam = -1;    
+
+    switch( Fam )
+    {
+    case AF_INET:
+        mt_fam = MT_PF_INET;
+        break;
+
+    case AF_INET6:
+        mt_fam = MT_PF_INET6;
+        break;
+
+    default:
+        perror("Invalid protocol family");
+    }
+
+    return mt_fam;
+}
+
+
+int 
+xe_net_get_native_protocol_family( mt_protocol_family_t Fam )
+{
+    int pfam = -1; 
+
+    switch( Fam )
+    {   
+    case MT_PF_INET:
+        pfam = AF_INET;
+        break;
+    case MT_PF_INET6:
+        pfam = AF_INET6;
+        break;
+    case MT_PF_UNSET:
+    default:
+        perror("Invalid protocol family");
+    }   
+
+    return pfam;
+}
+
+
+int 
+xe_net_get_native_sock_type( mt_sock_type_t Type )
+{
+    int stype = -1; 
+
+    switch( Type )
+    {   
+    case MT_ST_DGRAM:
+        stype = SOCK_DGRAM;
+        break;
+    case MT_ST_STREAM:
+        stype = SOCK_STREAM;
+        break;
+    case MT_ST_UNSET:
+    default:
+        //MYASSERT( !"Invalid socket type requested" );
+        perror("Invalid socket type requested");
+    }
+
+    return stype;
+}
+
+
+
 void
 populate_sockaddr_in( struct sockaddr_in * sockaddr,  
 		              mt_sockaddr_in_t *mt_sockaddr)
 {
-	    sockaddr->sin_family        = mt_sockaddr->sin_family;
+	    sockaddr->sin_family        = xe_net_get_native_protocol_family( mt_sockaddr->sin_family );
 		sockaddr->sin_port          = mt_sockaddr->sin_port;
 		sockaddr->sin_addr.s_addr   = mt_sockaddr->sin_addr.s_addr;
 		memcpy( sockaddr->sin_zero, mt_sockaddr->sin_zero, sizeof(sockaddr->sin_zero) );
