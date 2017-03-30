278e1a082baefe28d44faf6b83568b8efe3541cd
Matt Leinhos <matt@starlab.io>
2017-03-30 14:04:24 -0500

    Minor changes to performance testing

--- test/apache_bench/run_benchmark.sh
+++ test/apache_bench/run_benchmark.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-INDEXDIR=$MWROOT/protvm/user/http_server/
+INDEXDIR=/tmp
 FILEDIR=$PWD/files
 RESULTSDIR=$PWD/results
 TYPE=
+ATTEMPTS=1
 
 if [ ! -d "$FILEDIR" ]; then
     
@@ -16,26 +17,33 @@ fi
 
 
 if [ "$1" = "rump" ]; then
-
     ADDR=$RUMP_IP
     TYPE="rump"
 else
     ADDR=$PVM_IP
     TYPE="raw"
-fi
+Nfi
 
 ABS_PATH=$RESULTSDIR/$TYPE.dat
 mkdir -p $RESULTSDIR
 
-echo "#Size(bytes)      Average Response Time ( 10 reqests )" > $ABS_PATH
+echo "#Size(bytes)      Average Response Time ( $ATTEMPTS reqests )" > $ABS_PATH
 
 for i in $( ls $FILEDIR | sort -n ); do
     
     scp $FILEDIR/$i $USER@$PVM_IP:$INDEXDIR/index.html >> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Command failed"
+        break
+    fi
 
-    AVG_RESPONSE=`ab -n 10 "http://$ADDR/" | grep "\[ms\] (mean)" | awk '{ print $4 }'`
+    AVG_RESPONSE=`ab -n $ATTEMPTS "http://$ADDR/" | grep "\[ms\] (mean)" | awk '{ print $4 }'`
+    if [ $? -eq 0 ]; then
+        echo "Command failed"
+        break
+    fi
+
     SIZE=$i
-
     echo "$SIZE               $AVG_RESPONSE" >> $ABS_PATH
 
 done
