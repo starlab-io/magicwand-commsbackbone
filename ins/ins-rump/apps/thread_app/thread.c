089cf4a4a31e618c122859f5bc56756a7e0275fe
Matt Leinhos <matt@starlab.io>
2017-09-12 16:10:59 -0500

    Updated thread app to show possible ways to increase thread count.
    It looks like giving more memory to the instance will fix the problem and the solutions mentioned here are not needed.

--- ins/ins-rump/apps/thread_app/thread.c
+++ ins/ins-rump/apps/thread_app/thread.c
@@ -3,14 +3,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/sysctl.h>
 #include <sys/socket.h>
+#include <sys/resource.h>
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <pthread.h>
 #include "user_common.h"
 
 // Limit is 100. Teardown fails if it's 101.
-#define NUM_THREADS     100
+#define NUM_THREADS     200
 
 void error(const char *msg)
 {
@@ -27,14 +29,57 @@ void *PrintHello(void *threadid)
     pthread_exit(NULL);
 }
 
+
+int
+setlimit( void )
+{
+    int rc = 0;
+    int newval;
+    int oldval;
+    size_t newlen  = sizeof(newval);
+    size_t oldlen = sizeof(newval);
+#if 0
+    struct rlimit lim = { .rlim_cur = 500, .rlim_max = 500 };
+    rc = setrlimit( RLIMIT_NTHR, &lim );
+    if ( rc )
+    {
+        perror( "setrlimit" );
+    }
+    rc = setrlimit( RLIMIT_NPROC, &lim );
+    if ( rc )
+    {
+        perror( "setrlimit" );
+    }
+
+    // These are not working....
+    rc = sysctlbyname("kern.threads.max_threads_per_proc", &oldval, &oldlen, NULL, 0 );
+    if ( rc )
+    {
+        perror( "sysctlbyname" );
+    }
+    printf( "max_threads_per_proc: %d\n", oldval );
+
+    rc = sysctlbyname("kern.maxproc", &oldval, &oldlen, NULL, 0 );
+    if ( rc )
+    {
+        perror( "sysctlbyname" );
+    }
+    printf( "maxproc: %d\n", oldval );
+
+#endif
+    return 0;
+}
+
 int main ( void )
 {
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
-
+    
     printf( "In main\n" );
 
+    setlimit();
+    
     for ( t=0; t<NUM_THREADS; t++ )
     {
         printf("In main: creating thread %ld\n", t);
@@ -52,7 +97,7 @@ int main ( void )
     {
         pthread_join( threads[t], NULL );
     }
-    
-    return 0;
 
+ErrorExit:
+    return 0;
 }
