13ce0367af14de8edb95890a407aee9b7d21285b
Alex Speasmaker <alex.speasmaker@starlab.io>
2018-06-05 15:43:14 +0000

    Added performance testing docker containers

---
+++ test/docker_containers/apache2/build.sh
@@ -0,0 +1,3 @@
+#/bin/bash!
+
+sudo docker build -t apache2-performance-image .
---
+++ test/docker_containers/apache2/run.sh
@@ -0,0 +1,15 @@
+#/bin/bash!
+
+IMAGE_NAME=apache2-performance-image
+CONTAINER_NAME=apache2-performance-container
+
+sudo docker rm -f $CONTAINER_NAME
+
+sudo docker run						\
+	-dit 						\
+	--security-opt seccomp=unconfined               \
+	--privileged					\
+	--network=host					\
+	--device=/dev/mwcomms				\
+	--name $CONTAINER_NAME				\
+	$IMAGE_NAME
